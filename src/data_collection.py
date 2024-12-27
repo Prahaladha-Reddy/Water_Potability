ed3e423cc5f20d9667a6ddc7a704bbedbb0e93d5
Prahaladha-Reddy <prahaladreddy80@gmail.com>
2024-12-27 13:54:09 +0530

    The params.yaml is added

--- src/data_collection.py
+++ src/data_collection.py
@@ -3,10 +3,13 @@ import numpy as np
 import os
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
+import yaml
 
 data=pd.read_csv('C:/Users/bored/Music/Water_Potability/water_potability.csv')
 
-train,test=train_test_split(data,test_size=0.20,random_state=42)
+test_size=yaml.safe_load(open('params.yaml'))['data_collection']['test_size']
+
+train,test=train_test_split(data,test_size=test_size,random_state=42)
 
 data_path=os.path.join('data','raw')
 
--- src/model_building.py
+++ src/model_building.py
@@ -1,23 +1,48 @@
 import pandas as pd
 import numpy as np
-import os
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, RandomizedSearchCV
 from sklearn.ensemble import RandomForestClassifier
-import pickle
-
-train=pd.read_csv('C:/Users/bored/Music/Water_Potability/data/processed/train.csv')
-
-test=pd.read_csv('C:/Users/bored/Music/Water_Potability/data/processed/test.csv')
-
-x_train=train.drop(columns=['Potability'],axis=1)
-y_train=train['Potability']
-
-from sklearn.tree import DecisionTreeClassifier
-model = DecisionTreeClassifier()
-model.fit(x_train, y_train) 
-
-
+from sklearn.metrics import accuracy_score
 from joblib import dump
-dump(model, 'C:/Users/bored/Music/Water_Potability/model.joblib')
+import yaml
+
+params = yaml.safe_load(open('params.yaml'))
+test_size = params['data_collection']['test_size']
+random_search_params = params['model_building']['random_search']
+param_grid = random_search_params['param_grid']
 
 
+train = pd.read_csv('C:/Users/bored/Music/Water_Potability/data/processed/train.csv')
+test = pd.read_csv('C:/Users/bored/Music/Water_Potability/data/processed/test.csv')
+
+x_train = train.drop(columns=['Potability'], axis=1)
+y_train = train['Potability']
+
+x_test = test.drop(columns=['Potability'], axis=1)
+y_test = test['Potability']
+
+rf = RandomForestClassifier()
+
+random_search = RandomizedSearchCV(
+    estimator=rf,
+    param_distributions=param_grid,
+    n_iter=random_search_params['n_iter'],
+    cv=random_search_params['cv'],
+    verbose=2,
+    random_state=random_search_params['random_state'],
+    n_jobs=-1
+)
+
+print("Starting RandomizedSearchCV...")
+random_search.fit(x_train, y_train)
+print("RandomizedSearchCV completed.")
+
+
+best_params = random_search.best_params_
+print(f"Best Parameters: {best_params}")
+
+
+best_model = random_search.best_estimator_
+
+dump(best_model, 'C:/Users/bored/Music/Water_Potability/model.joblib')
+print("Model saved successfully.")
